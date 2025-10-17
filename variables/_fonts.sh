c8337099f82cedc107c7b708ef4bb5d14efdad19
Clebio <134241152+Clebio2030@users.noreply.github.com>
2025-10-17 00:35:38 -0300

    Update _fonts.sh

--- variables/_fonts.sh
+++ variables/_fonts.sh
@@ -8,6 +8,7 @@ readonly GREEN="\033[1;32m"
 readonly BLUE="\033[1;32m"
 readonly WHITE="\033[1;37m"
 readonly YELLOW="\033[1;33m"
+readonly ORANGE="\033[38;5;208m"   # laranja (ANSI 256)
 readonly GRAY_LIGHT="\033[0;37m"
 readonly CYAN_LIGHT="\033[1;36m"
 
